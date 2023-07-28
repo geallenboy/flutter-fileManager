0650d29d1f7fee384cfe856c83b1ed7c46a43f8d
戈嘉伦 <gejialun88@163.com>
2023-07-28 10:22:43 +0800

    movie

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,74 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:tesla_mobile_app_concept/screens/screens.dart';
+import 'package:photoplay_app/Providers/login_sharedpreference_provider.dart';
+import 'package:photoplay_app/Screens/login_screen.dart';
+import 'package:photoplay_app/Screens/navbar_roots.dart';
 import 'package:provider/provider.dart';
-import 'package:tesla_mobile_app_concept/util/price_tracker.dart';
+
+import 'Providers/theme_provider.dart';
 
 void main() {
-  //bool kReleaseMode = true;
-  //GitTest
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(
-      create: (context) => PriceTracker(),
-    )
-  ], child: const TeslaApp()));
+    ChangeNotifierProvider(create: (BuildContext context) => ThemeChangerProvider()),
+    ChangeNotifierProvider(create: (BuildContext context) => LoginSharedPreferenceProvider()),
+  ],
+      child: const MyApp()));
 }
 
-class TeslaApp extends StatelessWidget {
-  const TeslaApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  bool isLogin = false;
+
+  @override
+  void initState() {
+    Provider.of<LoginSharedPreferenceProvider>(context,listen: false).getValue();
+    super.initState();
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      builder: (context, child) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(fontFamily: 'Gotham Pro'),
-        home: const SplashScreen(),
-      ),
-      designSize: const Size(475, 1082),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context,snapshot){
+        if (snapshot.hasError) {
+          print("Something went wrong");
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        return Builder(builder: (BuildContext context){
+          return Consumer<LoginSharedPreferenceProvider>(
+            builder: (context, provider,_) {
+              return MaterialApp(
+                  debugShowCheckedModeBanner: false,
+                  title: 'Flutter Demo',
+                  themeMode: Provider.of<ThemeChangerProvider>(context).themeMode,
+                  theme: ThemeData(
+                      brightness: Brightness.light
+                  ),
+                  darkTheme: ThemeData(
+                    brightness: Brightness.dark,
+                  ),
+                  home: provider.getBoolLogin == true?const NavbarRoots():const LoginScreen()
+              );
+            }
+          );
+        });
+      },
+
     );
   }
 }
+
+
+
+
--- lib/screens/screens.dart
+++
@@ -1,4 +0,0 @@
-export 'main_screen.dart';
-export 'car_details.dart';
-export 'car_summary.dart';
-export 'splash_screen.dart';
