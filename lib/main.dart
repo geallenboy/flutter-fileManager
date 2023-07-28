a30bb51fbcc60734a9004c9bf8ee77ef74820e5c
戈嘉伦 <gejialun88@163.com>
2023-07-28 09:04:35 +0800

    coffee

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,37 @@
-import 'app/config/routes/app_pages.dart';
-import 'app/config/themes/app_theme.dart';
+import 'package:coffee_app/home.page.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+
+import 'config/services_locator.dart';
 
 void main() {
-  runApp(const MyApp());
+  initServicesLocator();
+  runApp(const CoffeApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class CoffeApp extends StatelessWidget {
+  const CoffeApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Project Management',
+    print("IZR / Flutter App Challenge - Coffee App (Flutter 3.7.1)");
+    return MaterialApp(
+      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: AppTheme.basic,
-      initialRoute: AppPages.initial,
-      getPages: AppPages.routes,
+      // showPerformanceOverlay: true,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
     );
   }
 }
